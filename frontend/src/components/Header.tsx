f77ff37a43a769af84b123f36f1e72716bdeb2b0
Rodrigo Moguillansky <romoguill@gmail.com>
2023-05-25 18:15:24 -0300

    Finished basic styling of header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,12 +1,16 @@
 import Avatar from './Avatar';
+import sunflower from '../assets/sunflower.svg';
 
 function Header() {
   return (
-    <header>
-      <div>
-        <h2>Agro Management</h2>
+    <header className='bg-gray-50 py-2 px-6 '>
+      <div className='flex justify-between items-center max-w-7xl mx-auto'>
+        <div className='flex items-center'>
+          <img className='h-12 w-12' src={sunflower} alt='agro app icon' />
+          <h2 className='text-2xl font-semibold text-gray-700'>AGRO</h2>
+        </div>
+        <Avatar fallback='RM' />
       </div>
-      <Avatar fallback='RM' />
     </header>
   );
 }
--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -3,10 +3,13 @@ import Header from '../components/Header';
 
 function MainLayout() {
   return (
-    <div>
+    <>
       <Header />
-      <Outlet />
-    </div>
+      <aside></aside>
+      <main>
+        <Outlet />
+      </main>
+    </>
   );
 }
 
