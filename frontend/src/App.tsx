ee15cea79069e3a196d7c23f85ca110cc1209f9f
Rodrigo Moguillansky <romoguill@gmail.com>
2023-05-25 09:54:10 -0300

    Setup basic client side routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,17 @@
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+import Home from './pages/Home';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path='/' element={<Home />} />)
+);
+
 function App() {
-  return <>hola</>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
