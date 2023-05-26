05df1cabe436986fa1c14a6a95b5a9327cd0879d
Rodrigo Moguillansky <romoguill@gmail.com>
2023-05-26 04:53:51 -0300

    Setup basic login page with its own route and layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,14 +8,19 @@ import Home from './pages/Home';
 import MainLayout from './layouts/MainLayout';
 import Dashboard from './pages/Dashboard';
 import Invoices from './pages/Invoices';
+import Login from './pages/Login';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<MainLayout />}>
-      <Route index element={<Home />} />
-      <Route path='dashboard' element={<Dashboard />} />
-      <Route path='invoices' element={<Invoices />} />
-    </Route>
+    <>
+      <Route path='/login' element={<Login />} />
+
+      <Route path='/' element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path='dashboard' element={<Dashboard />} />
+        <Route path='invoices' element={<Invoices />} />
+      </Route>
+    </>
   )
 );
 
