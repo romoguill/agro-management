49c35c80be611a85f2632a744d1b0a10d3442ef8
Rodrigo Moguillansky <romoguill@gmail.com>
2023-06-05 18:43:38 -0300

    Started shaping table using tanstack table

--- frontend/src/pages/MasterData/Suppliers.tsx
+++ frontend/src/pages/MasterData/Suppliers.tsx
@@ -1,4 +1,68 @@
+import {
+  createColumnHelper,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
+
+interface SupplierRow {
+  name: string;
+  description: string;
+  status: 'Active' | 'Inactive';
+}
+
+const demoSupplier: SupplierRow[] = [
+  {
+    name: 'YPF',
+    description: 'Fertilizers and herbicides',
+    status: 'Active',
+  },
+  {
+    name: 'Monsanto',
+    description: 'Seeds',
+    status: 'Inactive',
+  },
+];
+
 function Suppliers() {
-  return <div>Suppliers</div>;
+  const columnHelper = createColumnHelper<SupplierRow>();
+  const columns = [
+    columnHelper.accessor('name', {
+      cell: (info) => info.getValue(),
+    }),
+  ];
+
+  const table = useReactTable({
+    data: demoSupplier,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div>
+      <h1 className='text-4xl'>Suppliers</h1>
+    </div>
+  );
 }
 export default Suppliers;
+
+{
+  /* <table className='table'>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Description</th>
+      <th>Status</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>YPF</td>
+      <td>Fertilizers and herbicides</td>
+    </tr>
+    <tr>
+      <td>Monsanto</td>
+      <td>Seeds</td>
+    </tr>
+  </tbody>
+</table> */
+}
