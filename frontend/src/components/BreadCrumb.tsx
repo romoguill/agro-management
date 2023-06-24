ac1570176e1ad2b1b7080f840b85c3285986dc4c
Rodrigo Moguillansky <romoguill@gmail.com>
2023-06-24 16:51:57 -0300

    Created a separate component for breadcrumbs

---
+++ frontend/src/components/BreadCrumb.tsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+
+interface BreadCrumbProps {
+  location: string;
+  mainLevelName: string;
+  mainLevelPath: string;
+}
+
+function BreadCrumb({
+  location,
+  mainLevelName,
+  mainLevelPath,
+}: BreadCrumbProps) {
+  return (
+    <h1 className='text-2xl/none font-section-title font-bold text-gray-800 uppercase'>
+      <span className='text-xl text-gray-600 pr-2 capitalize'>
+        <Link to={`/${mainLevelPath}`} className='hover:text-primary pr-2'>
+          {mainLevelName}
+        </Link>
+        /
+      </span>
+      {location}
+    </h1>
+  );
+}
+export default BreadCrumb;
--- frontend/src/pages/MasterData/Suppliers.tsx
+++ frontend/src/pages/MasterData/Suppliers.tsx
@@ -1,9 +1,7 @@
 import { MdNoteAdd, MdSearch } from 'react-icons/md';
 import { Link } from 'react-router-dom';
-import Table, {
-  DataItem,
-  TableColumn,
-} from '../../components/TableData/TableData';
+import Table, { TableColumn } from '../../components/TableData/TableData';
+import BreadCrumb from '../../components/BreadCrumb';
 
 type Supplier = {
   name: string;
@@ -56,15 +54,12 @@ function Suppliers() {
   return (
     <div>
       <div className='flex justify-between items-center'>
-        <h1 className='text-2xl/none font-section-title font-bold text-gray-800'>
-          <span className='text-xl text-gray-600 pr-2'>
-            <Link to='/master-data' className='hover:text-primary pr-2'>
-              Master Data
-            </Link>
-            /
-          </span>
-          SUPPLIERS
-        </h1>
+        <BreadCrumb
+          location='suppliers'
+          mainLevelName='Master Data'
+          mainLevelPath='master-data'
+        />
+
         <div
           className='flex gap-2 justify-between items-center text-primary rounded-xl 
           bg-gray-100 px-3 py-2 font-semibold'
@@ -96,64 +91,6 @@ function Suppliers() {
         className='table-master-data text-left rounded-xl mt-5 overflow-hidden'
         isCheckable={true}
       />
-
-      {/* <table>
-        <thead>
-          <tr>
-            {headers.map((header) => (
-              <td key={header.key}>{header.label}</td>
-            ))}
-          </tr>
-        </thead>
-
-        <tbody>
-          {demoSupplier.map((supplier) => (
-            <tr key={supplier.name}>
-              <td>
-                <input type='checkbox' />
-              </td>
-              <td>{supplier.name}</td>
-              <td>{supplier.description}</td>
-              <td>{supplier.status}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table> */}
-
-      {/* <table className='table table-master-data text-left rounded-xl mt-5 overflow-hidden'>
-        <thead className='border-b-2 border-gray-400 text-lg'>
-          <tr>
-            <th className='w-3'>
-              <input type='checkbox' />
-            </th>
-            <th>Name</th>
-            <th>Description</th>
-            <th className='w-36'>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr className='border-t border-gray-200 hover:bg-gray-50'>
-            <td>
-              <input type='checkbox' />
-            </td>
-            <td className='font-bold'>YPF</td>
-            <td>Fertilizers and herbicides</td>
-            <td className='text-green-500' onClick={toggleStatus}>
-              Active
-            </td>
-          </tr>
-          <tr className='border-t border-gray-200 hover:bg-gray-50'>
-            <td>
-              <input type='checkbox' />
-            </td>
-            <td className='font-bold'>Monsanto</td>
-            <td>Seeds</td>
-            <td className='text-red-500' onClick={toggleStatus}>
-              Inactive
-            </td>
-          </tr>
-        </tbody>
-      </table> */}
     </div>
   );
 }
