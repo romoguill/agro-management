af77a4245320fa0b33d9d6a89e9f56ee9379a14d
Rodrigo Moguillansky <romoguill@gmail.com>
2023-07-03 20:12:12 -0300

    Created basic User related schemas types

---
+++ backend/src/schemas/userSchemas.ts
@@ -0,0 +1,30 @@
+import { z } from 'zod';
+import mongoose from 'mongoose';
+
+const User = z.object({
+  email: z.string().email({ message: 'Invalid email address' }),
+  firstName: z.string({ required_error: 'First name is required' }),
+  lastName: z.string({ required_error: 'Last name is required' }),
+  password: z
+    .string({ required_error: 'Password is required' })
+    .min(6, 'Password must be at least 6 characters'),
+});
+
+const hasPasswordConfirmation = z.object({
+  passwordConfirmation: z
+    .string({ required_error: 'Password is required' })
+    .min(6, 'Password must be at least 6 characters'),
+});
+
+const withId = z.object({ _id: z.instanceof(mongoose.Types.ObjectId) });
+
+const UserWithoutSensitiveData = User.omit({ password: true });
+
+const createUserPayload = User.merge(hasPasswordConfirmation);
+
+const UserWithId = User.merge(withId);
+
+export type User = z.infer<typeof User>;
+export type UserWithId = z.infer<typeof UserWithId>;
+export type UserWithoutSensitiveData = z.infer<typeof UserWithoutSensitiveData>;
+export type createUserPayload = z.infer<typeof createUserPayload>;
