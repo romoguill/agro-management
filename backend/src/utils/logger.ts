676a6591a461125efe8ff8a81918ef6755071d7c
Rodrigo Moguillansky <romoguill@gmail.com>
2023-07-07 12:28:15 -0300

    Modified logger so that is doesn't log to console during tests

--- backend/src/utils/logger.ts
+++ backend/src/utils/logger.ts
@@ -1,21 +1,37 @@
-import pino from 'pino';
 import path from 'path';
+import pino from 'pino';
 
-const transport = pino.transport({
-  targets: [
-    {
-      target: 'pino-pretty',
-      level: 'info',
-      options: { colorize: true },
-    },
-    {
-      target: 'pino/file',
-      level: 'info',
-      options: {
-        destination: path.join(__dirname, '../..', 'logs/server.log'),
+let transport = null;
+
+if (process.env.NODE_ENV === 'test') {
+  transport = pino.transport({
+    targets: [
+      {
+        target: 'pino/file',
+        level: 'info',
+        options: {
+          destination: path.join(__dirname, '../..', 'logs/server.log'),
+        },
       },
-    },
-  ],
-});
+    ],
+  });
+} else {
+  transport = pino.transport({
+    targets: [
+      {
+        target: 'pino-pretty',
+        level: 'info',
+        options: { colorize: true },
+      },
+      {
+        target: 'pino/file',
+        level: 'info',
+        options: {
+          destination: path.join(__dirname, '../..', 'logs/server.log'),
+        },
+      },
+    ],
+  });
+}
 
 export const logger = pino(transport);
