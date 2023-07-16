6df5063e5ea1c1977fe471fab79b029647559f20
Rodrigo Moguillansky <romoguill@gmail.com>
2023-07-16 18:00:20 -0300

    Created Auth context and its provider

---
+++ frontend/src/contexts/AuthContext.tsx
@@ -0,0 +1,53 @@
+import { ReactNode, createContext, useReducer } from 'react';
+
+export const AuthContext = createContext<{
+  user: User | null;
+  dispatch: React.Dispatch<AuthAction>;
+}>({ user: null, dispatch: () => null });
+
+enum AuthActionTypes {
+  LOGIN = 'LOGIN',
+  LOGOUT = 'LOGOUT',
+}
+
+export interface User {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  roles: string[];
+}
+
+interface AuthAction {
+  type: AuthActionTypes;
+  payload: User;
+}
+
+interface AuthState {
+  user: User | null;
+}
+
+const authReducer = (state: AuthState, action: AuthAction) => {
+  switch (action.type) {
+    case AuthActionTypes.LOGIN:
+      return { user: action.payload };
+    case AuthActionTypes.LOGOUT:
+      return { user: null };
+    default:
+      return state;
+  }
+};
+
+interface AuthContextProviderProps {
+  children: ReactNode;
+}
+
+export const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
+  const [state, dispatch] = useReducer(authReducer, { user: null });
+
+  return (
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
