5c7eaa2409b846e9d7735127a0a6887f4af7fc10
Rodrigo Moguillansky <romoguill@gmail.com>
2023-07-23 13:14:54 -0300

    Started making transition to shadcn components

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,29 +1,36 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
   theme: {
-    extend: {
-      colors: {
-        primary: {
-          DEFAULT: '#00ADB5',
-          50: '#6EF9FF',
-          100: '#59F8FF',
-          200: '#30F6FF',
-          300: '#08F4FF',
-          400: '#00D4DE',
-          500: '#00ADB5',
-          600: '#00777D',
-          700: '#004245',
-          800: '#000C0D',
-          900: '#000000',
-          950: '#000000',
-        },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
       },
     },
-    fontFamily: {
-      sans: ['Roboto', 'sans-serif'],
-      'section-title': ['Montserrat', 'sans-serif'],
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [],
-};
+  plugins: [require("tailwindcss-animate")],
+}
