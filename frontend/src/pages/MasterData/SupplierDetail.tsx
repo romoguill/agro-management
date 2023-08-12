b1955da75ad3befe9b6b5e50dc9a0cf54d805d97
Rodrigo Moguillansky <romoguill@gmail.com>
2023-08-12 17:02:59 -0300

    Added query logic for loading single supplier details into the supplier form. Uses initial data from the all supplier cache

--- frontend/src/pages/MasterData/SupplierDetail.tsx
+++ frontend/src/pages/MasterData/SupplierDetail.tsx
@@ -1,7 +1,39 @@
 import BreadCrumb from '@/components/BreadCrumb';
 import CreateSupplierForm from '@/components/MasterData/CreateSupplierForm';
+import useAuthContext from '@/hooks/useAuthContext';
+import { SupplierWithId } from '@/ts/interfaces';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import axios, { AxiosError } from 'axios';
+import { useParams } from 'react-router-dom';
+import { SpinnerCircularFixed } from 'spinners-react';
 
 function SupplierDetail() {
+  const { _id } = useParams();
+  const { auth } = useAuthContext();
+
+  const queryClient = useQueryClient();
+
+  const getSupplierById = async (id: string | undefined) => {
+    const response = await axios.get(`/api/suppliers/${id}`, {
+      headers: { Authorization: `Bearer ${auth.accessToken}` },
+    });
+    return response.data;
+  };
+
+  const { data, error, isLoading, isError } = useQuery<
+    SupplierWithId,
+    AxiosError
+  >({
+    queryKey: ['suppliers', _id],
+    queryFn: () => getSupplierById(_id),
+    initialData: () =>
+      queryClient
+        .getQueryData<SupplierWithId[]>(['suppliers'])
+        ?.find((supplier) => supplier._id === _id),
+    initialDataUpdatedAt: () =>
+      queryClient.getQueryState(['suppliers'])?.dataUpdatedAt,
+  });
+
   return (
     <div>
       <BreadCrumb
@@ -11,7 +43,25 @@ function SupplierDetail() {
       />
 
       <section className='mt-5'>
-        <CreateSupplierForm />
+        {isLoading && (
+          <SpinnerCircularFixed
+            size={30}
+            thickness={200}
+            style={{
+              color: '#d1d5db',
+              display: 'block',
+              margin: '2rem auto',
+            }}
+          />
+        )}
+
+        {isError && (
+          <p className='font-semibold text-slate-700 text-center my-2'>
+            Oops! There was an error while getting data
+          </p>
+        )}
+
+        {!isError && data && <CreateSupplierForm data={data} />}
       </section>
     </div>
   );
