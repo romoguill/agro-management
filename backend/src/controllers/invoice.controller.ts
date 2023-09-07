d091fa4e2a30dc12a01df18008e7feadf6c5ce33
Rodrigo Moguillansky <romoguill@gmail.com>
2023-09-07 09:26:17 -0300

    Finished with Invoice controller and adition shcemas for http

---
+++ backend/src/controllers/invoice.controller.ts
@@ -0,0 +1,85 @@
+import createHttpError from 'http-errors';
+import * as InvoiceService from '../services/invoice.service';
+import { NextFunction, Request, Response } from 'express';
+import {
+  RequestCreateInvoice,
+  RequestDeleteInvoice,
+  RequestGetInvoiceById,
+  RequestUpdateInvoice,
+} from '../schemas/invoice.schema';
+
+export const createInvoice = async (
+  req: Request<unknown, unknown, RequestCreateInvoice['body']>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const invoice = await InvoiceService.createInvoice(req.body);
+    res.status(201).json(invoice);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getAllInvoices = async (req: Request, res: Response) => {
+  const invoices = await InvoiceService.getAllInvoices();
+  res.status(200).json(invoices);
+};
+
+export const getInvoice = async (
+  req: Request<RequestGetInvoiceById['params']>,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+
+  try {
+    const invoice = await InvoiceService.getInvoiceById(id);
+    if (!invoice)
+      return next(createHttpError(404, `Invoice with id: ${id} not found`));
+
+    res.status(200).json(invoice);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateInvoice = async (
+  req: Request<
+    RequestUpdateInvoice['params'],
+    unknown,
+    RequestUpdateInvoice['body']
+  >,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+
+  try {
+    const invoice = await InvoiceService.updateInvoice(id, req.body);
+    if (!invoice)
+      return next(createHttpError(404, `Invoice with id: ${id} not found`));
+
+    res.status(200).json(invoice);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteInvoice = async (
+  req: Request<RequestDeleteInvoice['params']>,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+
+  try {
+    const invoice = await InvoiceService.deleteInvoice(id);
+    if (!invoice)
+      return next(createHttpError(404, `Invoice with id: ${id} not found`));
+
+    res.sendStatus(204);
+  } catch (error) {
+    next(error);
+  }
+};
--- backend/src/schemas/invoice.schema.ts
+++ backend/src/schemas/invoice.schema.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { z } from 'zod';
+import { objectIdValidator } from '../utils/idValidator';
 
 export const Currency = z.enum(['ARS', 'USD']);
 
@@ -17,3 +18,37 @@ export const Invoice = z.object({
 });
 
 export type Invoice = z.infer<typeof Invoice>;
+
+export const RequestCreateInvoice = z.object({
+  params: z.any().optional(),
+  query: z.any().optional(),
+  body: Invoice,
+});
+
+export type RequestCreateInvoice = z.infer<typeof RequestCreateInvoice>;
+
+export const RequestGetInvoiceById = z.object({
+  params: objectIdValidator(),
+  query: z.any().optional(),
+  body: z.any().optional(),
+});
+
+export type RequestGetInvoiceById = z.infer<typeof RequestGetInvoiceById>;
+
+export const RequestUpdateInvoice = z.object({
+  params: objectIdValidator(),
+  query: z.any().optional(),
+  body: Invoice.partial().refine((data) => Object.keys(data).length > 0, {
+    message: 'There were no fields specified to be updated',
+  }),
+});
+
+export type RequestUpdateInvoice = z.infer<typeof RequestUpdateInvoice>;
+
+export const RequestDeleteInvoice = z.object({
+  params: objectIdValidator(),
+  query: z.any().optional(),
+  body: z.any().optional(),
+});
+
+export type RequestDeleteInvoice = z.infer<typeof RequestDeleteInvoice>;
